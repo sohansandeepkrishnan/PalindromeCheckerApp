5110caf3f2fddfe9babffc410c9f35b3158d118b
sohansandeepkrishnan <sohan.skpv@gmail.com>
2026-02-25 09:25:56 +0530

    UC2: Hardcoded palindrome validation using character comparison

---
+++ src/Uc2PalindromeCheckerApp.java
@@ -0,0 +1,21 @@
+public class Uc2PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String word = "madam";
+        boolean isPalindrome = true;
+        int left = 0;
+        int right = word.length() - 1;
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
+    }
+}
